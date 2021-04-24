b9c05864d2416c7227258c20e4e95546d6e328e5
Martin Walther <it@martin-walther.ch>
2021-04-24 16:59:28 +0200

    Add Python Docstring

--- Classes/PyFileFolder.py
+++ Classes/PyFileFolder.py
@@ -3,7 +3,8 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 class colors():
-    ''' colors '''
+    """define colors
+    """
     black      = '\033[30m'
     red        = '\033[31m'
     green      = '\033[32m'
@@ -23,22 +24,33 @@ class colors():
 
 
 class pyFileFolder:
-    '''File and folder class'''
+    """File and folder class
+    """
     pass
 
     # Constructor   
     def __init__(self) -> None:
+        """Constructor of class
+        """
         pass
 
-    def list_directory(self, dirpath):
-        '''List the content of a directory '''
-        dirs = os.listdir(dirpath)
+    def list_directory(self, path):
+        """List the content of a directory, this can be files and subfolders
+
+        Args:
+            path (string): path to the directory
+        """
+        dirs = os.listdir(path)
         print('{0}{1}{2}'.format(colors.purple, dirs, colors.reset))
 
 
-    def list_tree(self, dirpath):
-        '''Walk trough a tree''' 
-        for root, directories, files in os.walk(dirpath, topdown=False):
+    def list_tree(self, path):
+        """Walk trough a directory tree
+
+        Args:
+            path (string): path to the directory
+        """
+        for root, directories, files in os.walk(path, topdown=False):
             for name in files:
                 fullname = os.path.join(root, name)
                 print('{0}file:   {1}{2}'.format(colors.purple, fullname, colors.reset))
@@ -47,9 +59,14 @@ class pyFileFolder:
                 print('{0}folder: {1}{2}'.format(colors.purple, fullname, colors.reset))
 
 
-    def file_exists(self, dirpath, file):
-        '''Check if file exists'''
-        fullname = os.path.join(dirpath,file)
+    def file_exists(self, path, file):
+        """Check if the file exists
+
+        Args:
+            path (string): path to the directory
+            file (string): name of the file to check
+        """
+        fullname = os.path.join(path,file)
         if os.path.exists(fullname):
             print('{0}{1} exists{2}'.format(colors.purple, fullname, colors.reset))
         else:
@@ -57,7 +74,15 @@ class pyFileFolder:
 
 
     def get_latest_file(self, path, pattern):
-        '''Returns the name of the latest (most recent) file of the joined path(s)'''
+        """Check for the latest (most recent) file of the path
+
+        Args:
+            path    (string): path to the directory
+            pattern (string): pattern to search for
+
+        Returns:
+            string: the name of the latest (most recent) file
+        """
         fullpath = os.path.join(path, pattern)
         files = glob.glob(fullpath)
         if not files: 
@@ -67,7 +92,15 @@ class pyFileFolder:
 
 
     def get_sorted_list_of_files(self,path,pattern):
-        '''Returns a dict of files'''
+        """Get a sorted list of files
+
+        Args:
+            path    (string): path to the directory
+            pattern (string): pattern to search for
+
+        Returns:
+            list: a list of files with the properties LastWriteTime, Name, FullName
+        """
         all_files    = glob.glob(os.path.join(path, pattern))
         files_sorted = sorted(all_files, key=lambda t: -os.stat(t).st_mtime)
         file_object  = []
@@ -84,8 +117,17 @@ class pyFileFolder:
         return file_object
 
 
-    def get_latest_number_of_files(self,path,pattern,days_before):
-        '''Returns a dict of files newer than day x'''
+    def get_latest_number_of_files(self,path,pattern,days):
+        """Get a list with sorted files newer than specified days
+
+        Args:
+            path (string): path to the directory
+            pattern (string): pattern to search for
+            days (int): number of days
+
+        Returns:
+            list: a list of files with the properties LastWriteTime, Name, FullName
+        """
         all_files    = glob.glob(os.path.join(path, pattern))
         files_sorted = sorted(all_files, key=lambda t: -os.stat(t).st_mtime)
         file_object  = []
@@ -93,7 +135,7 @@ class pyFileFolder:
             modTimesinceEpoc = os.path.getctime(file)
             modificationtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(modTimesinceEpoc))
             file_datetime    = datetime.strptime(modificationtime, "%Y-%m-%d %H:%M:%S")
-            if file_datetime > days_before:
+            if file_datetime > days:
                 file_dict = {
                     'LastWriteTime' : modificationtime,
                     'Name'          : os.path.basename(file),
@@ -109,43 +151,47 @@ def main(dirpath):
     '''main function'''
 
     # Get the current direcory
-    print('{0}Location: {1}{2}'.format(colors.green, os.getcwd(), colors.reset))
+    print('{0}Current location{1}'.format(colors.green, colors.reset))
+    print('{0}{1}{2}'.format(colors.purple, os.getcwd(), colors.reset))
+
+    # Execute functions with directory path
+    print('{0}Directory{1}'.format(colors.green, colors.reset))
+    print('{0}{1}{2}'.format(colors.purple, dirpath, colors.reset))
 
     # Create an instance of the class
     dir = pyFileFolder()
 
-    # Execute functions of the class
-    print('{0}Directory: {1}{2}'.format(colors.purple, dirpath, colors.reset))
-
-    # Get a list of files in the directory
+    # 1. Get a list of files in the directory
     print('{0}{1}{2}'.format(colors.green, dir.list_directory.__doc__, colors.reset))
     dir.list_directory(dirpath)
 
-    # Get a tree list of the directory
+    # 2. Get a tree list of the directory
     print('{0}{1}{2}'.format(colors.green, dir.list_tree.__doc__, colors.reset))
     dir.list_tree(dirpath)
 
-    # Check if a file exists
+    # 3. Check if a file exists
     print('{0}{1}{2}'.format(colors.green, dir.file_exists.__doc__, colors.reset))
     dir.file_exists(dirpath,'vscode.png')
 
-    # Get the latest file of a directory
+    # 4. Get the latest file of a directory
     print('{0}{1}{2}'.format(colors.green, dir.get_latest_file.__doc__, colors.reset))
     print('{0}{1}{2}'.format(colors.purple, dir.get_latest_file(dirpath, '*'), colors.reset))
 
-    # Get sorted file list
+    # 5. Get sorted file list
+    print('{0}{1}{2}'.format(colors.green, dir.get_sorted_list_of_files.__doc__, colors.reset))
     filelist = dir.get_sorted_list_of_files("/Users/Tinu/Temp/", '*.*')
     for file in filelist:
         print(f"{file['LastWriteTime']}\t{file['Name']}\t{file['FullName']}")
     
-    # Get all files greater than 3 days before
+    # 6. Get all files greater than 3 days before
+    print('{0}{1}{2}'.format(colors.green, dir.get_latest_number_of_files.__doc__, colors.reset))
     day3before = (datetime.now() - timedelta(days=3))
     filelist = dir.get_latest_number_of_files("/Users/Tinu/Temp/", '*.*', day3before)
     for file in filelist:
         print('{0}{1}{2}'.format(colors.yellow, file['LastWriteTime'] + ', ' + file['Name']+ ', ' + file['FullName'], colors.reset))
 
-    #df = pd.DataFrame(filelist)
-    #print('{0}{1}{2}'.format(colors.yellow, df, colors.reset))
+    df = pd.DataFrame(filelist)
+    print('{0}{1}{2}'.format(colors.yellow, df, colors.reset))
 
 # Call the main function
 if __name__ =='__main__':
