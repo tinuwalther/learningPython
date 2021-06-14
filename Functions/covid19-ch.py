ef3a55e96165d12a1d487abbf6df6d57edb1b5ae
Martin Walther <it@martin-walther.ch>
2021-06-14 14:24:30 +0200

    Update

--- Functions/covid19-ch.py
+++ Functions/covid19-ch.py
@@ -1,24 +1,25 @@
-import requests 
+import requests
 
 def send_discord_message(data, date):
     '''Send Discord message'''
-    webhook_url     = 'https://discordapp.com/api/webhooks/851028735331008512/JCYUCmlSfAm_Cl0d6mQxyx45RXZae1xR-OU0FIRA8hp1hoMNQzlHx8dHcVSiKeTjB4Fb'
-    author_name     = 'Covid19 Hook'
-    author_avatar   = 'https://i2.pngguru.com/preview/189/787/605/covid19-coronavirus-corona-violet-pink-purple-cartoon-magenta-smile-png-clipart-thumbnail.jpg'
-    section_title   = '[INFO] COVID-19 Statistics for SWITZERLAND'
+    webhook_url         = "https://discordapp.com/api/webhooks/851028735331008512/JCYUCmlSfAm_Cl0d6mQxyx45RXZae1xR-OU0FIRA8hp1hoMNQzlHx8dHcVSiKeTjB4Fb"
+    author_name         = "Covid19 Hook"
+    author_avatar       = "https://i2.pngguru.com/preview/189/787/605/covid19-coronavirus-corona-violet-pink-purple-cartoon-magenta-smile-png-clipart-thumbnail.jpg"
+    section_title       = "[INFO] COVID-19 Statistics for SWITZERLAND"
+    section_description = "Information on the current situation, as of " + date
 
     embeds = {
-        'title': section_title, 'description': f'Information on the current situation, as of {date}', "color": 32767,
-        'fields': [
-            {"name" : 'Confirmed Cases', "value" : data['Cases'], "inline": "true"},
-            {"name" : 'Hospitalisations', "value" : data['Hospitalisations'], "inline": "true"},
-            {"name" : 'Deaths', "value" : data['Deaths'], "inline": "true"},
+        "title": section_title, "description": section_description, "color": 32767,
+        "fields": [
+            {"name" : "Confirmed Cases", "value" : data["Cases"], "inline": "true"},
+            {"name" : "Hospitalisations", "value" : data["Hospitalisations"], "inline": "true"},
+            {"name" : "Deaths", "value" : data["Deaths"], "inline": "true"},
             {"name" : "Official website", "value" : "[Federal Office of Public Health FOPH | Bundesamt für Gesundheit BAG](https://www.covid19.admin.ch/en/overview?ovTime=total)"}
         ]
     }
 
     data = {
-        'username': author_name, 'avatar_url': author_avatar, "embeds": [embeds],
+        "username": author_name, "avatar_url": author_avatar, "embeds": [embeds],
     }
 
     headers = {
@@ -27,69 +28,69 @@ def send_discord_message(data, date):
 
     result = requests.post(webhook_url, json=data, headers=headers)
     if 200 <= result.status_code < 300:
-        print('[INFO] Webhook sent {0}'.format(result.status_code))
+        print("[INFO] Webhook sent {0}".format(result.status_code))
     else:
-        print('[WARN] Not sent with {0}, response: {1}'.format(result.status_code, result.json()))
+        print("[WARN] Not sent with {0}, response: {1}".format(result.status_code, result.json()))
 
 # Covid19-CH API
 def get_data_version(url):
     '''return JSON-file'''
     from bson.json_util import loads
-    response = requests.get(url +'/context')
+    response = requests.get(url +"/context")
     if(response.status_code == 200):
-        html_content = response.content.decode('utf-8')
+        html_content = response.content.decode("utf-8")
         json_data = loads(html_content)
-        if 'dataVersion' in json_data:
-            data = json_data['dataVersion']
-            return f'{data}'
+        if "dataVersion" in json_data:
+            data = json_data["dataVersion"]
+            return data
         else:
-            print('dataVersion not found')
+            print("dataVersion not found")
     else:
         print(response.status_code)
 
 def get_source_date(url):
     '''return JSON-file'''
     from bson.json_util import loads
-    response = requests.get(url +'/context')
+    response = requests.get(url +"/context")
     if(response.status_code == 200):
         html_content = response.content.decode('utf-8')
         json_data = loads(html_content)
-        if 'sourceDate' in json_data:
-            data = json_data['sourceDate']
-            return f'{data}'
+        if "sourceDate" in json_data:
+            data = json_data["sourceDate"]
+            return data
         else:
-            print('sourceDate not found')
+            print("sourceDate not found")
     else:
         print(response.status_code)
 
 def get_apidata(url, type, last_update):
     '''return JSON'''
     from bson.json_util import loads
-    data_json_url = f'{url}/sources/COVID19{type}_geoRegion.json'
+    data_json_url = f"{url}/sources/COVID19{type}_geoRegion.json"
     #print(data_json_url)
     response = requests.get(data_json_url)
     if(response.status_code == 200):
-        html_content = response.content.decode('utf-8')
+        html_content = response.content.decode("utf-8")
         json_data = loads(html_content)
         for row in json_data:
-            if row['geoRegion'] == 'CH':
-                if row['datum'] == last_update:
-                    return f'Total: {row["sumTotal"]}\nToday: {row["entries_diff_last"]}'
+            if row["geoRegion"] == "CH":
+                if row["datum"] == last_update:
+                    return "Total: " + str(row["sumTotal"]) + "\nToday: " + str(row["entries_diff_last"])
 
-if __name__ =='__main__':
-    api_url      = 'https://www.covid19.admin.ch/api/data'
+if __name__ =="__main__":
+    api_url      = "https://www.covid19.admin.ch/api/data"
     data_version = get_data_version(api_url)
     source_date  = get_source_date(api_url)
     last_update = source_date[0:10]
 
-    data_version_url = f'{api_url}/{data_version}'
+    data_version_url = api_url + "/" + data_version
     if data_version_url:
-        cases = get_apidata(data_version_url, 'Cases', last_update)
-        hosp  = get_apidata(data_version_url, 'Hosp', last_update)
-        death = get_apidata(data_version_url, 'Death', last_update)
+        cases = get_apidata(data_version_url, "Cases", last_update)
+        hosp  = get_apidata(data_version_url, "Hosp", last_update)
+        death = get_apidata(data_version_url, "Death", last_update)
         discord_data = {
-            'Cases': f'{cases}',
-            'Hospitalisations': f'{hosp}',
-            'Deaths': f' {death}',
+            "Cases": cases,
+            "Hospitalisations": hosp,
+            "Deaths": death,
         }
         send_discord_message(discord_data, last_update)
